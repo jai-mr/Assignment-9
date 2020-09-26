11aa1b9edfc5cf6cbe3f420ed6e3117ebec21a55
jai-mr <68414965+jai-mr@users.noreply.github.com>
2020-09-26 17:58:47 +0530

    Delete summary.py

--- packages/summary.py
+++
@@ -1,9 +0,0 @@
-import torch
-import resnet as m
-from torchsummary import summary
-
-def summ(device):
-
-    print(device)
-    model = m.ResNet18().to(device)
-    summary(model, input_size=(3, 32, 32))
